b69a8291b822d45c42cb1e181d2efb237b811e0f
Seppis <roopes16@hotmail.com>
2024-02-27 19:21:32 +0200

    First bach of exercises

---
+++ L01/L01_T2.py
@@ -0,0 +1,5 @@
+num1 = 50
+num2 = 120
+print(num1, num2)
+print(num1, "Tämä teksti tulee muuttujien väliin", num2)
+print("Uniikki unikorni olikin ainoastaan korni koni")
---
+++ L01/L01_T3.py
@@ -0,0 +1,11 @@
+luku = 5
+vaihe1 = luku + 34
+vaihe2 = vaihe1 * 3
+lopputulos = vaihe2 -6
+print(lopputulos)
+
+print((luku + 34) * 3 - 6)
+
+luku1 = 13
+luku2 = 2
+print(luku1 % luku2)
